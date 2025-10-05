880f115da9d69923ec4513f522bab6e585ebc95c
rijaskhan <rijassali15@gmail.com>
2025-10-05 11:48:54 +0530

    Modify workflow and tests

--- utils/schema_validator.py
+++ utils/schema_validator.py
@@ -1,10 +1,24 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
+class MainData(BaseModel):
+    temp: float
+    feels_like: Optional[float]
+    temp_min: Optional[float]
+    temp_max: Optional[float]
+    pressure: Optional[int]
+    humidity: Optional[int]
+
+class WeatherDescription(BaseModel):
+    id: int
+    main: str
+    description: str
+    icon: str
+
 class WeatherSchema(BaseModel):
     name: str
-    weather: List[dict]
-    main: dict
+    weather: List[WeatherDescription]
+    main: MainData
     sys: dict
 
 class CountrySchema(BaseModel):
